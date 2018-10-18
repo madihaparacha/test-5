870d73d0bf7300c4979f4b2cd42a02617d24adb4
madihaparacha <38943126+madihaparacha@users.noreply.github.com>
2018-10-18 17:54:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.carousel');
+//     var instances = M.Carousel.init(elems, options);
+//   });
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
